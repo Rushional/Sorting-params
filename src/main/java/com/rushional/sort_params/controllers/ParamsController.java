578745c043fefd30c300430ff8452c4dcbbf80cc
Rushional <rushional2@gmail.com>
2024-04-16 20:32:22 +0400

    Getting a map and a string of query params

--- src/main/java/com/rushional/sort_params/controllers/ParamsController.java
+++ src/main/java/com/rushional/sort_params/controllers/ParamsController.java
@@ -1,13 +1,19 @@
 package com.rushional.sort_params.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class ParamsController {
 
     @PostMapping("/params")
-    public String sortParams() {
-        return "I'm totally a hash of a JSON of sorted param name-value pairs. Like, totally.";
+    public String sortParams(@RequestParam Map<String,String> allRequestParams) {
+        return allRequestParams.keySet().stream()
+                .map(key -> key + "=" + allRequestParams.get(key))
+                .collect(Collectors.joining(", ", "{", "}"));
     }
 }
