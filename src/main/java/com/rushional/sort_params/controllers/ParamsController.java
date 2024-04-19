5ec2d9521f13787269a50ae9562200b77e65108c
Rushional <rushional2@gmail.com>
2024-04-19 17:51:56 +0400

    Added swagger

--- src/main/java/com/rushional/sort_params/controllers/ParamsController.java
+++ src/main/java/com/rushional/sort_params/controllers/ParamsController.java
@@ -1,11 +1,9 @@
 package com.rushional.sort_params.controllers;
 
 import com.rushional.sort_params.dtos.OperationResponse;
-import com.rushional.sort_params.exceptions.InternalErrorException;
 import com.rushional.sort_params.services.facades.ParamsFacade;
+import io.swagger.annotations.ApiImplicitParam;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +18,8 @@ public class ParamsController {
     private final ParamsFacade paramsFacade;
 
     @GetMapping("/params/{operationId}")
+    @ApiImplicitParam(name = "Token", value = "Access Token", allowEmptyValue = true,
+            paramType = "header", dataTypeClass = String.class)
     public OperationResponse sortParams(@PathVariable("operationId") Long operationId,
                                            @RequestParam Map<String,String> allRequestParams) {
         return paramsFacade.paramsMapToHashedString(allRequestParams);
